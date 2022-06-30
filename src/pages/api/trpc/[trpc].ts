119fc277e728813b8993cd11ed3ded9c516de331
Mikołaj Rosowski <m.rosowski1@wp.pl>
2022-06-30 19:13:20 +0200

    setup tRPC

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,18 @@
+import * as trpc from "@trpc/server";
+import * as trpcNext from "@trpc/server/adapters/next";
+import { prisma } from "../../../db/client";
+
+export const appRouter = trpc.router().query("getAllGames", {
+  async resolve() {
+    return await prisma.game.findMany();
+  },
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
+
+// export API handler
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  createContext: () => null,
+});
